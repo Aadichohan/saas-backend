4bb7b415abacfafc4f4caec2cb4b79da36b1bb0e
AbdulAhad <49091109+Aadichohan@users.noreply.github.com>
2026-03-11 16:56:44 +0500

    worked on url

--- saas_project/urls.py
+++ saas_project/urls.py
@@ -4,8 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.http import JsonResponse, HttpResponsePermanentRedirect
 
 urlpatterns = [
+    # path('', lambda request: JsonResponse({})),
+    path('', lambda request: HttpResponseRedirect('admin/')),
     path('admin/', admin.site.urls),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
